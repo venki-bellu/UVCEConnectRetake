42b412208fc2f161984e26de5bd557f595964aa5
allanakshay12 <allanakshay@outlook.com>
2017-07-18 16:33:30 +0530

    Few Optimizations

--- app/src/main/java/com/venkibellu/myapplication/News_Adapter.java
+++ app/src/main/java/com/venkibellu/myapplication/News_Adapter.java
@@ -6,7 +6,7 @@ import android.content.Context;
 
 import android.net.Uri;
 
-import android.support.v7.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +15,7 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.TextView;
+
 
 
 import com.google.android.gms.tasks.OnSuccessListener;
