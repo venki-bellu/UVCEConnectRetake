b2d7bef4efae9471c4d0438822fabc11149648a2
Suhas <suhas1048@gmail.com>
2017-07-19 15:07:55 +0530

    Added drawer image and made card views for about section as well, nned to add parent for about activity

--- app/src/main/java/com/venkibellu/myapplication/about_us.java
+++ app/src/main/java/com/venkibellu/myapplication/about_us.java
@@ -2,6 +2,7 @@ package com.venkibellu.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class about_us extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class about_us extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
